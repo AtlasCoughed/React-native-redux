c1af796fb282ae6936133872ad4b8f424c58b494
Michael Tran <michaelhuytran@gmail.com>
2017-03-19 19:51:10 -0700

    [feature] - button creation

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,10 +2,17 @@ import React from "react";
 import { Text, View, Image } from "react-native";
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
 	const { title, artist, thumbnail_image, image } = album;
-	const {thumbnailStyle, headerContentStyles, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
+	const {
+		thumbnailStyle,
+		headerContentStyles,
+		thumbnailContainerStyle,
+		headerTextStyle,
+		imageStyle
+	} = styles;
 
 	return (
 		<Card>
@@ -25,6 +32,9 @@ const AlbumDetail = ({ album }) => {
 				<Image
 					style={imageStyle}
 					source={{uri: image}}/>
+			</CardSection>
+			<CardSection>
+				<Button />
 			</CardSection>
 		</Card>
 	)
